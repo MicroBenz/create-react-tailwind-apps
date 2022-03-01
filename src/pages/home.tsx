412d5a2800d6a16c0ad24406628a4421bafbbec7
Tananan Tangthanachaikul <microbenz.prob@gmail.com>
2022-03-01 16:42:24 +0700

    Feature: Update dependencies

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const HomePage = () => {
   const [count, setCount] = useState(0);
   return (
-    <div>
-      <h1 className="text-blue-500 text-2xl">Home Page</h1>
+    <div className="container mx-auto">
+      <h1 className="text-2xl text-blue-500">Home Page</h1>
       <p>{count}</p>
       <button className="block" onClick={() => setCount(c => c + 1)}>Inc</button>
       <Link to="/about-us">Go to about us</Link>
